6920ae6c392f275e547580e1c298a0f0b69fe679
vishnuxx <vishnuvs2002it@gmail.com>
2023-05-17 22:33:40 +0530

    setup page routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './styles/App.css';
+import { Route, Routes } from "react-router";
+import "./styles/App.css";
+import LandingPage from "./Pages/LandingPage/LandingPage";
+import { ROUTES } from "./Constants/RouterPaths";
 
 function App() {
   return (
     <div className="App">
-     
+      <Routes>
+        <Route path={ROUTES.LANDING_PAGE} element={<LandingPage />} />
+      </Routes>
     </div>
   );
 }
