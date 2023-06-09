00809ae2c776ae940dcf1be820e525c49fe5b24b
vishnuxx <vishnuvs2002it@gmail.com>
2023-06-10 00:27:32 +0530

    merged responsive nav menu into single component

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,43 @@
-import useMediaQuery from "../../hooks/useMediaQuery";
-import NavbarDesktop from "./Navbar.Desktop";
-import NavbarMobile from "./Navbar.Mobile";
+import HighlightButton from "../Highlightbutton/HighlightButton";
+import Logo from "../Logo/Logo";
+// import globalstyle from "../../style/globals.module.css";
+import style from "./navbar.module.css";
+import { HamburgerIcon } from "../../Icons/HamburgerIcon";
+import useNavBar from "../../hooks/useNavbar";
+import { CloseIcon } from "../../Icons/CloseIcon";
 
-function Navbar() {
-  const isDesktop = useMediaQuery("(min-width: 960px)");
+function NavbarDesktop() {
+  const [isOpen, setOpen] = useNavBar();
+  console.log(isOpen);
   return (
-    <>
-      {isDesktop? <NavbarDesktop></NavbarDesktop> : <NavbarMobile></NavbarMobile>}
-    </>
+    <div className={style.header}>
+      <a href="" className={style.logo}>
+        <Logo style={{ display: "flex" }}></Logo>
+      </a>
+      <input className={style.menuBtn} type="checkbox" id="menu-btn" />
+      <label className={style.menuIcon} htmlFor="menu-btn">
+        <span className={style.navicon}></span>
+      </label>
+      <ul className={style.menu}>
+        <li>
+          <a href="#work">Home</a>
+        </li>
+
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#careers">Events</a>
+        </li>
+        <li>
+          <a href="#contact">Media</a>
+        </li>
+      </ul>
+      <div style={{ marginRight: "20px" }}>
+        <HighlightButton>Build a Startup</HighlightButton>
+      </div>
+    </div>
   );
 }
-export default Navbar;
+
+export default NavbarDesktop;
